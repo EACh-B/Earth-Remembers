cc63bf94980428c43d985935785f40ecb30ca05a
Anderson-RC <ross-anderson@msn.com>
2018-04-18 11:04:31 +0100

    Folded Asymptotic probability into the game
    Code lies within TippingPointController.
    Changes made to the tipping points min/max in the Main Scene's unity editor to match the values Manjana provided. Appears to work as expected however the slope value of 0.5 may be a bit too harsh when you playtest it.

--- Earth Remembers/Earth Remembers/Assets/Scripts/Tipping Points/TippingPointController.cs
+++ Earth Remembers/Earth Remembers/Assets/Scripts/Tipping Points/TippingPointController.cs
@@ -3,11 +3,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Linq;
+using System;
+
 
 public class TippingPointController : MonoBehaviour {
 
     bool coralReef, arcticIce, boreal, monsoon, antarcticIce;
     public List<float> TPChances = new List<float>(3);
+    /// <summary>
+    /// Floor value for probability.
+    /// </summary>
+    public float baseProb = 0;
+    /// <summary>
+    /// Affects the abruptness of the transition from low probability to high. Between 0 and 1.
+    /// This is the primary tweakable variable in this.
+    /// If we want separate slopes for different environmental issues this will require refactoring.
+    /// </summary>
+    public float slope = 0.5f;
+    /// <summary>
+    /// The number of data points we want to include in this mapping. Set this high enough that the probabilities are acceptably approximated
+    /// Probabilities are calculated in these discrete steps. Continuous temperature values are binned into their closest discrete step
+    /// </summary>
+    public int stepCount = 50;
+
     WorldStatusController wsc;
 
     //For getting from WSC
@@ -53,7 +72,7 @@ public class TippingPointController : MonoBehaviour {
 
         if (value >= min)
         {
-            TPChances[4] = (value / (max - min)) * 100;
+            TPChances[4] = GetProbability(value,min,max) * 100;
         }
 
         int rnd = UnityEngine.Random.Range(1, 100);
@@ -78,7 +97,7 @@ public class TippingPointController : MonoBehaviour {
 
         if (value >= min)
         {
-            TPChances[3] = (value / (max - min)) * 100;
+            TPChances[3] = GetProbability(value, min, max) * 100;
         }
 
         int rnd = UnityEngine.Random.Range(1, 100);
@@ -103,7 +122,7 @@ public class TippingPointController : MonoBehaviour {
 
         if(value >= min)
         {
-            TPChances[0] = (value / (max - min)) * 100;
+            TPChances[0] = GetProbability(value, min, max) * 100;
         }
 
         int rnd = UnityEngine.Random.Range(1, 100);
@@ -127,7 +146,7 @@ public class TippingPointController : MonoBehaviour {
 
         if (value >= min)
         {
-            TPChances[1] = (value / (max - min)) * 100;
+            TPChances[1] = GetProbability(value, min, max) * 100;
         }
 
         int rnd = UnityEngine.Random.Range(1, 100);
@@ -152,7 +171,7 @@ public class TippingPointController : MonoBehaviour {
 
         if (value >= min)
         {
-            TPChances[2] = (value / (max - min)) * 100;
+            TPChances[2] = GetProbability(value, min, max) * 100;
         }
 
         int rnd = UnityEngine.Random.Range(1, 100);
@@ -169,5 +188,100 @@ public class TippingPointController : MonoBehaviour {
             img.color = Color.green;
             img.gameObject.transform.GetChild(0).GetComponent<Text>().text = TPChances[2].ToString("0.0") + "%";
         }
+    }
+
+    /////Ross' main addition to this class
+
+    public float GetProbability(float currentWorldTemp, float minTemp, float maxTemp)
+    {
+        //If we exceed the graph boundaries this will asymptote
+        if (currentWorldTemp >= maxTemp) { return 1.0f; };
+        if (currentWorldTemp <= minTemp) { return 0.0f; };
+        float range = maxTemp - minTemp;
+        if (minTemp > maxTemp)
+        {
+            throw new System.Exception
+                ("GenerateRampedThreshold arg error: minimum value is greater than maximum. Min: " + minTemp.ToString("0.00") + " Max: " + maxTemp.ToString("0.00"));
+        }
+
+        List<float> temperatures = fspan(minTemp, maxTemp, this.stepCount);
+        if (currentWorldTemp >= temperatures.Last()) { return 1.0f; };
+        if (currentWorldTemp <= temperatures.First()) { return 0.0f; };
+
+        //ramp = ((temperatures-tmin) + abs(temperatures-tmin))/2.
+        List<float> ramp = GenerateRamp(temperatures, minTemp);
+        //ramped_thresh = ramp / (tmax - tmin)
+        List<float> rampedThresholds = ramp.Select(i => i / range).ToList<float>();
+        //tanh_ramp = tanh((ramped_thresh + baseprob) / slope)
+        List<float> tanhRamp = rampedThresholds.Select(i => (float)Math.Tanh((i + baseProb) / slope)).ToList<float>();
+        //Get the closest index for the temperature so we can use that index in the same sized list of probabilities(tanhRamp)
+        int probabilityIndex = GetApproximatedTemperatureIndex(currentWorldTemp, temperatures);
+        var dic = temperatures.Select((k, i) => new { k, v = tanhRamp[i] })
+              .ToDictionary(x => x.k, x => x.v);
+        return tanhRamp[probabilityIndex];
+    }
+    /// <summary>
+    /// Returns the index of the closest value to the provided world temperature within the list of temperatures.
+    /// </summary>
+    /// <param name="currentWorldTemp">Temperature we want to approximate</param>
+    /// <param name="temperatures"></param>
+    /// <returns>the index of the closest value to the provided world temperature</returns>
+    private int GetApproximatedTemperatureIndex(float currentWorldTemp, List<float> temperatures)
+    {
+        int closestIndex = 0;
+        //Assert the world temp is within the temperature bounds
+        if (!(currentWorldTemp >= temperatures.First() && currentWorldTemp <= temperatures.Last()))
+        { throw new System.Exception("Passed in value outside the range of temperatures"); } //This should never happen as we filter out-of-range temps earlier
+        if (currentWorldTemp == temperatures.First()) { return 0; }
+        for (int i = 1; i < temperatures.Count; ++i)
+        {
+            if (currentWorldTemp <= temperatures[i])
+            {
+                if (currentWorldTemp == temperatures[i]) { return i; }
+                float a = temperatures[i];
+                float b = temperatures[i - 1];
+                float deltaA = Mathf.Abs(a - currentWorldTemp);
+                float deltaB = Mathf.Abs(b - currentWorldTemp);
+                closestIndex = deltaA < deltaB ? i : (i + 1);
+                return closestIndex;
+            }
+        }
+        return closestIndex;
+    }
+    /// <summary>
+    /// Implementation of fspan, which returns an evenly spaced list of values stepCount+1 long, between min and max inclusive
+    /// </summary>
+    /// <param name="min">Lower bound value must be < max</param>
+    /// <param name="max">Upper bound value must be > min</param>
+    /// <param name="stepCount">'Resolution' of the steps from min->max</param>
+    /// <returns>an evenly spaced list of values stepCount+1 long, between min and max inclusive</returns>
+    private static List<float> fspan(float min, float max, int stepCount)
+    {
+        if (min > max) { throw new System.Exception("fspan arg error: minimum value is greater than maximum. Min: " + min.ToString("0.00") + " Max: " + max.ToString("0.00")); }
+        if (stepCount <= 0) { throw new System.Exception("stepCount must be greater than 0. Provided stepcount: " + stepCount.ToString()); }
+        List<float> spanList = new List<float>();
+        float stepSize = (max - min) / (float)stepCount;
+        for (int i = 0; i <= stepCount; i++)
+        {
+            spanList.Add(min + (float)i * stepSize);
+        }
+        return spanList;
+    }
+    /// <summary>
+    /// Moved to its own method because, assuming .NET 3.5, this line of the formula is quite verbose.
+    /// ramp = ((temperatures - tmin) + abs(temperatures - tmin)) / 2.
+    /// </summary>
+    private List<float> GenerateRamp(List<float> temperatures, float tmin)
+    {
+        //ramp = ((temperatures - tmin) + abs(temperatures - tmin)) / 2.
+        List<float> temperaturesMinusTMin = temperatures.Select(i => i - tmin).ToList<float>();
+        List<float> abstemperaturesMinusTMin = temperaturesMinusTMin.Select(i => Mathf.Abs(i)).ToList<float>();
+        //Enumerable.Zip is not supported in .NET 3.5 so performing a zip manually
+        List<float> ramp = new List<float>();
+        for (int i = 0; i < temperaturesMinusTMin.Count; i++)
+        {
+            ramp.Add((temperaturesMinusTMin[i] + abstemperaturesMinusTMin[i]) / 2.0f);
+        }
+        return ramp;
     }
 }
